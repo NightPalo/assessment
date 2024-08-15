5f933202c57c70270e58e20b6f396554873888a4
ZNightZ <rolyalnight@gmail.com>
2024-08-15 20:00:49 +0700

    easy pasit

--- assessment/src/component/card/CardContainer.tsx
+++ assessment/src/component/card/CardContainer.tsx
@@ -1,34 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { fircardContent, secoundCardContent } from "./cardContent";
+import { Box } from "@mui/material";
 
 export const CardContainer: React.FC = () => {
+  const [currentCard, setCurrentCard] = useState(1);
   return (
     <Card sx={{ minWidth: 600, minHeight: 500 }} style={{ borderRadius: 40 }}>
-      <CardContent style={{ paddingTop: 100 }}>
-        <Typography gutterBottom variant="h4">
-          โลมาเป็นปลาหรือไม่
-        </Typography>
-      </CardContent>
-      <CardActions
-        style={{
-          flex: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 100,
-        }}
-      >
-        <Button variant="contained" style={{ margin: 20, minWidth: 100 }}>
-          <Typography variant="h6">ใช่</Typography>
-        </Button>
-        <Button variant="contained" style={{ margin: 20, minWidth: 100 }}>
-          <Typography variant="h6">ไม่ใช่</Typography>
-        </Button>
-      </CardActions>
+      {currentCard === 1 && (
+        <Box>
+          <CardContent style={{ paddingTop: 100 }}>
+            <Typography gutterBottom variant="h4">
+              {fircardContent.header}
+            </Typography>
+            <Typography gutterBottom variant="h4">
+              {fircardContent.subHeader}
+            </Typography>
+            <Box style={{ paddingTop: 100 }}>
+              <Typography gutterBottom variant="h5">
+                {fircardContent.description}
+              </Typography>
+              <Typography gutterBottom variant="h5">
+                {fircardContent.subDescription}
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions
+            style={{
+              flex: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 100,
+            }}
+          >
+            <Button
+              variant="contained"
+              style={{ margin: 20, minWidth: 100, borderRadius: 30 }}
+              onClick={() => setCurrentCard(2)}
+            >
+              <Typography variant="h6" style={{ margin: 10 }}>
+                {fircardContent.button}
+              </Typography>
+            </Button>
+          </CardActions>
+        </Box>
+      )}
+      {currentCard === 2 && (
+        <Box>
+          <CardContent style={{ paddingTop: 100 }}>
+            <Typography gutterBottom variant="h3" style={{ padding: 30 }}>
+              {secoundCardContent.title}
+            </Typography>
+            <Typography gutterBottom variant="h4" style={{ paddingTop: 20 }}>
+              {secoundCardContent.content1}
+            </Typography>
+            <Typography gutterBottom variant="h4" style={{ paddingTop: 20 }}>
+              {secoundCardContent.content2}
+            </Typography>
+            <Typography gutterBottom variant="h4" style={{ paddingTop: 20 }}>
+              {`${secoundCardContent.content3}${secoundCardContent.content4}${secoundCardContent.content5}`}
+            </Typography>
+            <Typography gutterBottom variant="h4" style={{ paddingTop: 20 }}>
+              {secoundCardContent.content6}
+            </Typography>
+            <Typography gutterBottom variant="h4" style={{ paddingTop: 20 }}>
+              {secoundCardContent.content7}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h4"
+              style={{ paddingTop: 20 }}
+            ></Typography>
+          </CardContent>
+          <CardActions
+            style={{
+              flex: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 100,
+            }}
+          >
+            <Button
+              variant="contained"
+              style={{ margin: 20, minWidth: 100, borderRadius: 30 }}
+              onClick={() => setCurrentCard(2)}
+            >
+              <Typography variant="h6" style={{ margin: 10 }}>
+                {secoundCardContent.button}
+              </Typography>
+            </Button>
+          </CardActions>
+        </Box>
+      )}
     </Card>
   );
 };
